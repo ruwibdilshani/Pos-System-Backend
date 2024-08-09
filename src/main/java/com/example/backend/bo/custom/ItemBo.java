5f54459a2a46100c046d52099e153e2caa43f307
ruwibdilshani <ruwibdilshani0921@gmail.com>
2024-08-09 12:39:30 +0530

    ITEMDone

---
+++ src/main/java/com/example/backend/bo/custom/ItemBo.java
@@ -0,0 +1,17 @@
+package com.example.backend.bo.custom;
+
+import com.example.backend.bo.SuperBO;
+import com.example.backend.dto.ItemDto;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface ItemBo extends SuperBO {
+    boolean addItem(ItemDto itemDto) throws SQLException;
+    ItemDto searchItem(String id) throws SQLException;
+    boolean updateItem(ItemDto itemDto) throws SQLException;
+    boolean deleteItem(String i) throws SQLException;
+    List<ItemDto> getAllItems() throws SQLException;
+
+
+}
