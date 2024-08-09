ba04560e774686c43fa70fda7cb250110d42807d
ruwibdilshani <ruwibdilshani0921@gmail.com>
2024-08-09 12:40:55 +0530

    orderDaoImpl Done

--- src/main/java/lk/ijse/possystembackend/dao/OrderDetailsDAO.java
+++
@@ -1,7 +0,0 @@
-package lk.ijse.possystembackend.dao;
-
-import lk.ijse.possystembackend.dto.OrderDetailDTO;
-
-public sealed interface OrderDetailsDAO permits OrderDetailsDAOIMPL{
-    String saveOrderDetails(OrderDetailDTO orderDetail);
-}
