622e4957e56e2ee08912951897d9bff9bb92a774
ruwibdilshani <ruwibdilshani0921@gmail.com>
2024-08-09 12:39:46 +0530

    ITEMDone

---
+++ src/main/java/com/example/backend/dao/custom/impl/ItemDaoImpl.java
@@ -0,0 +1,79 @@
+package com.example.backend.dao.custom.impl;
+
+import com.example.backend.dao.custom.ItemDao;
+import com.example.backend.db.ConnectionManager;
+import com.example.backend.dto.ItemDto;
+import com.example.backend.entity.Item;
+import com.example.backend.utill.SQLUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ItemDaoImpl implements ItemDao {
+
+    Connection connection;
+    PreparedStatement preparedStatement;
+
+    @Override
+    public boolean save(Item item) throws SQLException {
+        return SQLUtil.execute("INSERT INTO item VALUES (?,?,?,?)",
+                item.getId(),
+                item.getName(),
+                item.getQty(),
+                item.getPrice()
+        );
+    }
+
+    @Override
+    public Item getData(String id) throws SQLException {
+        ResultSet resultSet = SQLUtil.execute("SELECT * FROM item WHERE item_id=?", id);
+        if (resultSet.next()) {
+            return new Item(
+                    resultSet.getString(1),
+                    resultSet.getString(2),
+                    resultSet.getInt(3),
+                    resultSet.getDouble(4)
+            );
+        }
+        return null;
+    }
+
+    @Override
+    public boolean update(Item item) throws SQLException {
+        return SQLUtil.execute("UPDATE item SET item_name = ? , item_qty = ? , item_price = ? WHERE item_id=?",
+                item.getName(),
+                item.getQty(),
+                item.getPrice(),
+                item.getId()
+        );
+    }
+
+    @Override
+    public boolean delete(String id) throws SQLException {
+        return SQLUtil.execute("DELETE FROM item WHERE item_id=?", id);
+    }
+
+    @Override
+    public List<Item> getAll() throws SQLException {
+        ResultSet resultSet = SQLUtil.execute("SELECT * FROM item");
+        List<Item> itemList = new ArrayList<>();
+        while (resultSet.next()) {
+            itemList.add(new Item(
+                    resultSet.getString(1),
+                    resultSet.getString(2),
+                    resultSet.getInt(3),
+                    resultSet.getDouble(4)
+            ));
+        }
+        return itemList;
+    }
+
+    @Override
+    public boolean updateQty(String id, int qty) throws SQLException {
+        return SQLUtil.execute("UPDATE item SET item_qty = item_qty - ? WHERE item_id=?", qty, id);
+    }
+}
---
+++ src/main/java/com/example/backend/dto/ItemDto.java
@@ -0,0 +1,16 @@
+package com.example.backend.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ItemDto {
+    private String id;
+    private String name;
+    private int qty;
+    private double price;
+}
