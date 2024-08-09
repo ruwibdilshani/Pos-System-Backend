8acffeeb59c66095c0190b86de5bd1baefb805cc
ruwibdilshani <ruwibdilshani0921@gmail.com>
2024-08-09 12:40:42 +0530

    orderDaoImpl Done

--- src/main/java/lk/ijse/possystembackend/dao/OrderDAO.java
+++
@@ -1,7 +0,0 @@
-package lk.ijse.possystembackend.dao;
-
-import lk.ijse.possystembackend.dto.OrderDTO;
-
-public sealed interface OrderDAO permits OrderDAOIMPL{
-    String saveOrder(OrderDTO order);
-}
